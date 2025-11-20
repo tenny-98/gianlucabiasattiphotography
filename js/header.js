8fef49d982fbcff353c2888cfd506f91e86458a0
gianlucabiasatti <biasattig@libero.it>
2025-11-20 22:57:08 +0100

    Update header.js

--- js/header.js
+++ js/header.js
@@ -1,1 +1,11 @@
-x
+**
+ * Carica header.html dinamicamente
+ */
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('html/header.html')
+        .then(response => response.text())
+        .then(html => {
+            document.body.insertAdjacentHTML('afterbegin', html);
+        })
+        .catch(err => console.error('Errore caricamento header:', err));
+});

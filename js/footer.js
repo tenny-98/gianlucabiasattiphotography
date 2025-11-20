00b1522de93d73eb10c560d05fa72595ae2527a6
gianlucabiasatti <biasattig@libero.it>
2025-11-20 22:57:24 +0100

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -1,1 +1,11 @@
-y
+/**
+ * Carica footer.html dinamicamente
+ */
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('html/footer.html')
+        .then(response => response.text())
+        .then(html => {
+            document.body.insertAdjacentHTML('beforeend', html);
+        })
+        .catch(err => console.error('Errore caricamento footer:', err));
+});
